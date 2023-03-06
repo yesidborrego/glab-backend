1bc193ccd4c310652eb0528aae29401a4b0453b5
Yesid <yesidborrego@gmail.com>
2023-03-06 15:25:28 -0500

    Credentials db

--- src/db/configDb.js
+++ src/db/configDb.js
@@ -6,6 +6,12 @@ const { dbOptions } = require("./");
 //   host: dbOptions.DATABASE_HOST,
 //   dialect: dbOptions.DATABASE_DIALECT,
 // });
-const db = new Sequelize(dbOptions.DATABASE, dbOptions.DATABASE_USER, dbOptions.DATABASE_PASSWORD, { dialect: "mysql"});
+
+console.log("dbOptions.PORT:", dbOptions.DATABASE_PORT)
+const db = new Sequelize(dbOptions.DATABASE, dbOptions.DATABASE_USER, dbOptions.DATABASE_PASSWORD, {
+  host: dbOptions.DATABASE_HOST,
+  dialect: "mysql",
+  port: dbOptions.DATABASE_PORT,
+});
 
 module.exports = db;
